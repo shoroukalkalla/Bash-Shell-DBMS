1881639eadd9c71908de356cbe630bdc2ee09b18
mohamed159716 <mohamed159716@yahoo.com>
2022-01-20 15:56:36 +0200

    Created Meta data of table structure

---
+++ createMetaData.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+
+function createMetaData {
+	#currentDir=$1
+	currentDir=DB/iti/students/
+	#colsNumber=$2
+	colsNumber=2
+	
+	#mkdir $currentDir/metaData.db
+
+	if [ -s t ]
+	then
+		echo "Yes"
+	else
+		echo "No"
+	fi
+}
+
+createMetaData
