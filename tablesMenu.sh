4d3bdfa68d8989f677ed1bf9508ab32dbd79cfbf
shoroukalkalla <shroukalkalla1997@gmail.com>
2022-01-20 17:17:02 +0200

    Updated tablesMenu file

--- tablesMenu.sh
+++ tablesMenu.sh
@@ -16,13 +16,13 @@ function tablesMenu {
 	select choice in "${options[@]}"
 	do
 		case $REPLY in
-			list | 1) echo list;;
-			create | 2) echo create;;
+			list | 1) ls DB/iti/.;;
+			create | 2) createTable.ss;;
 			insert | 3) echo insert;;
 			select | 4) echo select;;
 			update | 5) echo update;;
 			delete | 6) echo delete;;
-			drop | 7) echo drop;;
+			drop | 7) ./dropTables.sh;;
 			back | 8) echo back;;
 			exit | 9) exit;;
 			*) echo $REPLY is not of the choices;;
